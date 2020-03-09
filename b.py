7455d7e58c2c9eb5cfefc8a2d17559a73ba9fcc3
shanaka95 <shanaka95@mail.com>
2020-03-09 12:09:04 +0530

    asdasda

--- b.py
+++ b.py
@@ -15,16 +15,4 @@ PARAMS = {'address':location}
 r = requests.get(url = URL, params = PARAMS) 
   
 # extracting data in json format 
-data = r.json() 
-  
-  
-# extracting latitude, longitude and formatted address  
-# of the first matching location 
-latitude = data['results'][0]['geometry']['location']['lat'] 
-longitude = data['results'][0]['geometry']['location']['lng'] 
-formatted_address = data['results'][0]['formatted_address'] 
-  
-# printing the output 
-print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
-      %(latitude, longitude,formatted_address)) 
-
+print(r)
