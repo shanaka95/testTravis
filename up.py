e366b32c0fbae38fa186c7373f48b9614326da54
shanaka95 <shanaka95@mail.com>
2020-05-03 14:27:26 +0530

    test

--- up.py
+++ up.py
@@ -8,7 +8,11 @@ g_login.CommandLineAuth()
 #g_login.Authorize()
 drive = GoogleDrive(g_login)
 for i in os.listdir(os.getcwd()+"/downloads"): 
-	upload = drive.CreateFile({'title': i})
-	upload.SetContentFile(os.getcwd()+"/downloads/"+i)
-	upload.Upload()
-	print ('uploade d')
+	try:
+		if os.path.isfile(os.getcwd()+"/downloads/"+i)
+			upload = drive.CreateFile({'title': i})
+			upload.SetContentFile(os.getcwd()+"/downloads/"+i)
+			upload.Upload()
+			print ('uploade d')
+	except:
+		pass
